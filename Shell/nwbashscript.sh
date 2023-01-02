e14b4b0410e50277e0b04b0878188b0413860c63
Olavo Borges <120344319+LordGriffo@users.noreply.github.com>
2023-01-02 19:08:20 -0300

    Add files via upload

---
+++ Shell/nwbashscript.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Testar se usuário passou o número certo de
+#Argumentos... (1 argumento)
+
+[[ $# -ne 1 ]] && echo "Epa! Eu Preciso de *um* nome de arquivo!" && exit 1
+
+
+#Testar se Arquivo já existe....
+
+[[ -f $1 ]] && echo "Epa! Arquivo ja existe! Saindo!" && exit 1
+
+
+#Colocar o Enviroment na hashbang ((Executar no pré-shell)
+
+echo '#!/usr/bin/env bash' > $1
+chmod +x $1
+nano $1
